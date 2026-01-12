6c10c193bfc81a1233022ef5b31eb7b821cebb60
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-01-12 13:20:14 -0500

    l2/2 - express multi url app w/nodemon for autorestart

--- src/express-server.ts
+++ src/express-server.ts
@@ -5,11 +5,26 @@ const app: any = express();
 
 // set up middleware to handle & route http req / res
 const hello = (req: Request, res: Response): void => {
-    res.status(200).send('<h1>Hello World</h1>');
+    res.status(200).send('<h1>Hello Express</h1>');
 };
 
+const goodbye = (req: Request, res: Response): void => {
+    res.status(200).send('<h1>Goodbye Express</h1>');
+};
+
+const home = (req: Request, res: Response): void => {
+    if (req.path === '/') {
+        res.status(200).send('<h1>Home</h1>');
+    }
+    else {
+        res.status(404).send('<h1>Not Found</h1>');
+    }
+}
+
 // url dispatching
-app.use(hello);
+app.use('/hello', hello);
+app.use('/goodbye', goodbye);
+app.use('/', home);
 
 app.listen(4000);
 console.log('Express running on port 4000');
